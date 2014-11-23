aa1f7e355ebb5ecef20625a1c653c0e272e34553
ZZKer <Kyle.Mumenthaler@gmail.com>
2014-11-23 00:11:50 -0600

    V1.00: It works; no pointer paths though

--- index.js
+++ index.js
@@ -4,6 +4,17 @@
  * @module Sequencer
  * @author ZZKer
  * @desc Ordered Auto-Sequencer
+ * @version 1.00
+ * 
+ * TODO
+ *  - Make multiple sequence paths
+ *  - Add seeded random module for paths
+ *  - Make paths user friendly
+ * 
+ * Much code is currently commented out to try and
+ * fix major issues with arrays. Apparently setting and
+ * reading double arrays doesn't really work correctly?
+ * Idk. Let me know if you figure anything out.
  */
 
 //default sequence. Set to noise() for debugging
@@ -19,27 +30,45 @@ export function Sequencer() {
   Sequencer.prototype.addcur = 0;//address of current sequence
   Sequencer.prototype.addtab = [defal];//address table for all sequences
   Sequencer.prototype.lentab = [1];//length table for all sequences
-  Sequencer.prototype.pnttab = [[0]];//pointer table for next sequences
+  //Sequencer.prototype.pnttab = [[0]];//pointer table for next sequences
 }
 
+/**
 Sequencer.prototype.add = function(nextseq, nextlen, nextpnt){
   var l = this.addtab.length;
   if(l < 2){//if first addition, change pointer table values
     this.pnttab[0] = [1];
   }
+  */
+Sequencer.prototype.add = function(nextseq, nextlen){
+  var l = this.addtab.length;
   this.addtab[l] = nextseq;//add sequence
   this.lentab[l] = nextlen;//add length
-  this.pnttab[l] = nextpnt;//add pointers
+  //this.pnttab[l] = nextpnt;//add pointers
 };
 
 Sequencer.prototype.play = function(t){
+  if(t === 0){
+    this.addcur = 0;
+    this.curt = 0;
+    this.endt = 1;
+  }
   if(t-this.curt > this.endt){//if sequence reaches length time
-    var l = this.pnttab[this.addcur].length;
+    /**var l = this.pnttab[this.addcur].length;
     //the current sequence becomes a sequence randomly chosen from the given pointers
     addcur = this.pnttab[this.addcur][Math.floor(Math.random()*l)];
+    
+    //commented out until I figure out why this doesn't work
+    */
+    
+    //This simple fix removes multiple sequence paths
+    this.addcur++;
+    if(this.addcur >= this.addtab.length){
+      this.addcur = 1;
+    }
     this.curt = t;//start time becomes the current time
     //end time becomes length time from current
-    this.endt = this.curt + this.lentab[this.addcur];
+    this.endt = this.lentab[this.addcur];
   }
   //play the current sequence with the sequence time
   return this.addtab[this.addcur](t-this.curt);
--- test.js
+++ test.js
@@ -1,22 +1,38 @@
 
 /**
  * test
- * stuff is messed up
- * not sure why nothing is working right
- * If you know how to fix, please submit a bug report on github
+ * Currently, multiple sequence paths does not work.
+ * There is a problem with using double arrays that
+ * I need to figure out how to fix.
+ * If you know how to fix, please submit a bug report on github.
+ * 
+ * If you want to mess around with the pointer table I made,
+ * be sure NOT to hit the play button, as it can cause
+ * wavepot to crash. If that happens, just reload the page and
+ * don't hit the play button for this module. However, if you
+ * don't mess around with that stuff, then it should work fine.
  */
 
 import { sin, saw, ramp, tri, sqr, pulse, noise } from 'opendsp/osc';
 import Sequencer from './index.js';
 
 var examp = new Sequencer();
+/**
 examp.add(first, 1, [2]);
 examp.add(second, 1, [3]);
 examp.add(third, 1, [4]);
 examp.add(forth, 1, [0,1,2,3,4]);
+*/
+examp.add(first, 1.75);
+examp.add(second, 1);
+examp.add(third, 2.25);
+examp.add(forth, 3);
+
+//volume
+var v = 0.1;
 
 export function dsp(t) {
-  return examp.play(t);
+  return v * examp.play(t);
 }
 
 function first(t) {
@@ -24,7 +40,7 @@ function first(t) {
 }
 
 function second(t) {
-  return saw(t, 320);
+  return saw(t, 400);
 }
 
 function third(t) {
@@ -32,5 +48,5 @@ function third(t) {
 }
 
 function forth(t) {
-  return tri(t, 320);
+  return tri(t, 290);
 }

fe0e7070d98fc8f629dfc4135f6ac9498275af6b
ZZKer <Kyle.Mumenthaler@gmail.com>
2014-11-23 01:44:16 -0600

    V1.01: fixes; multiple sequencers; setintro

--- index.js
+++ index.js
@@ -4,47 +4,39 @@
  * @module Sequencer
  * @author ZZKer
  * @desc Ordered Auto-Sequencer
- * @version 1.00
+ * @version 1.01
  * 
  * TODO
  *  - Make multiple sequence paths
  *  - Add seeded random module for paths
  *  - Make paths user friendly
- * 
- * Much code is currently commented out to try and
- * fix major issues with arrays. Apparently setting and
- * reading double arrays doesn't really work correctly?
- * Idk. Let me know if you figure anything out.
  */
 
-//default sequence. Set to noise() for debugging
-export var defal = function(t){return Math.random()*2-1;};
-//export var defal = function(t){return 0;};
+//default sequence. Set to noise for debugging purposes
+export var noise = function(t){return Math.random()*2-1;};
+export var emptyfunk = function(t){return 0;};
 
 export default Sequencer;
 
 export function Sequencer() {
   if (!(this instanceof Sequencer)) return new Sequencer();
-  Sequencer.prototype.curt   = 0;//current sequence start: t=0
-  Sequencer.prototype.endt   = 1;//end of current sequence
-  Sequencer.prototype.addcur = 0;//address of current sequence
-  Sequencer.prototype.addtab = [defal];//address table for all sequences
-  Sequencer.prototype.lentab = [1];//length table for all sequences
-  //Sequencer.prototype.pnttab = [[0]];//pointer table for next sequences
+  this.curt   = 0;//current sequence start: t=0
+  this.endt   = 1;//end of current sequence
+  this.addcur = 0;//address of current sequence
+  this.addtab = [emptyfunk];//address table for all sequences
+  this.lentab = [1];//length table for all sequences
 }
 
-/**
-Sequencer.prototype.add = function(nextseq, nextlen, nextpnt){
-  var l = this.addtab.length;
-  if(l < 2){//if first addition, change pointer table values
-    this.pnttab[0] = [1];
-  }
-  */
+//For replacing the intro
+Sequencer.prototype.setintro = function(seq, len){
+  this.addtab[0] = seq;
+  this.lentab[0] = len;
+};
+
+//For adding sequences to the end of the Sequencer
 Sequencer.prototype.add = function(nextseq, nextlen){
-  var l = this.addtab.length;
-  this.addtab[l] = nextseq;//add sequence
-  this.lentab[l] = nextlen;//add length
-  //this.pnttab[l] = nextpnt;//add pointers
+  this.addtab[this.addtab.length] = nextseq;//add sequence
+  this.lentab[this.lentab.length] = nextlen;//add length
 };
 
 Sequencer.prototype.play = function(t){
@@ -54,22 +46,32 @@ Sequencer.prototype.play = function(t){
     this.endt = 1;
   }
   if(t-this.curt > this.endt){//if sequence reaches length time
-    /**var l = this.pnttab[this.addcur].length;
-    //the current sequence becomes a sequence randomly chosen from the given pointers
-    addcur = this.pnttab[this.addcur][Math.floor(Math.random()*l)];
-    
-    //commented out until I figure out why this doesn't work
-    */
-    
-    //This simple fix removes multiple sequence paths
-    this.addcur++;
+    this.addcur++;//Increase to the next sequence
     if(this.addcur >= this.addtab.length){
-      this.addcur = 1;
+      if(this.addtab.length == 1){
+        this.addcur = 0;//replay sequence if only intro
+      }else {
+        this.addcur = 1;//replay to sequence 1 if after end
+      }
     }
     this.curt = t;//start time becomes the current time
-    //end time becomes length time from current
+    //end time becomes length of current sequence
     this.endt = this.lentab[this.addcur];
   }
   //play the current sequence with the sequence time
   return this.addtab[this.addcur](t-this.curt);
 };
+
+/**
+ * Version Change Log
+ * V1.01
+ *  - Changed .prototypes. vars to this. vars in constructor
+ *    This fixes no multiple Sequencers issue
+ *  - Changed .add to not use var l
+ *  - Changed notes on test
+ *  - Set emptyfunk to own function
+ *  - Removed all code relating to broken pointer table
+ *  - Added check for intro-only Sequencer in .play
+ *  - Added .setintro to change sequence 0
+ *  - Added example of anon func use in test
+ */
--- test.js
+++ test.js
@@ -1,38 +1,45 @@
 
 /**
  * test
- * Currently, multiple sequence paths does not work.
- * There is a problem with using double arrays that
- * I need to figure out how to fix.
- * If you know how to fix, please submit a bug report on github.
+ * Currently, multiple sequence pathing is not implimented.
+ * The old system does not work properly.
+ * If you know how to impliment, please fork and submit
+ * a pull request.
  * 
- * If you want to mess around with the pointer table I made,
- * be sure NOT to hit the play button, as it can cause
- * wavepot to crash. If that happens, just reload the page and
- * don't hit the play button for this module. However, if you
- * don't mess around with that stuff, then it should work fine.
+ * Also fair to note:
+ * -  The Sequencer is not good for fly-by edits.
+ * It is best to test a single sequence in another tab
+ * then copy it over when it sounds right.
+ * Changing any code will restart the Sequencer.
+ * -  The Sequencer has caused wavepot client crashes while testing.
+ * If the sounds skips indefinitely, reload the page.
  */
 
 import { sin, saw, ramp, tri, sqr, pulse, noise } from 'opendsp/osc';
-import Sequencer from './index.js';
+import { Sequencer, emptyfunk } from './index.js';
 
 var examp = new Sequencer();
-/**
-examp.add(first, 1, [2]);
-examp.add(second, 1, [3]);
-examp.add(third, 1, [4]);
-examp.add(forth, 1, [0,1,2,3,4]);
-*/
-examp.add(first, 1.75);
+examp.add(first, 2);
 examp.add(second, 1);
-examp.add(third, 2.25);
-examp.add(forth, 3);
+examp.add(third, 1);
+examp.add(forth, 2);
+examp.add(emptyfunk, 2);
+
+examp.setintro(intro, 1);
+
+var exemp = new Sequencer();
+exemp.setintro(emptyfunk, 1);
+exemp.add(first, 1);
 
 //volume
 var v = 0.1;
 
 export function dsp(t) {
-  return v * examp.play(t);
+  return v * (examp.play(t) + exemp.play(t)*0.1);
+}
+
+function intro(t) {
+  return sin(t, 320 * t);
 }
 
 function first(t) {
